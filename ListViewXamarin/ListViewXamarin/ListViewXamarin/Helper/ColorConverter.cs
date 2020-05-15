653f6bc6d17c630d352035ed9d5112b29e5e3b3c
Jayaleshwari N <jayaleshwarin@syncfusion.com>
2020-05-15 12:46:07 +0530

    XAMARIN-34616 - Sample and ScreenShot added.

---
+++ ListViewXamarin/ListViewXamarin/ListViewXamarin/Helper/ColorConverter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using Xamarin.Forms;
+
+namespace ListViewXamarin
+{
+    public class ColorConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return (bool)value ? parameter as Grid != null ? Color.Blue : Color.Yellow : Color.Transparent;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
